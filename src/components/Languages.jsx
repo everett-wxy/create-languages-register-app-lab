8720dc283c00db20356f4a2f3de54c09ef7ca399
everett-wxy <74697318+everett-wxy@users.noreply.github.com>
2024-10-25 01:22:14 +0800

    Programmed functionality: - Retrieve all languages - Delete a language - Add a language

---
+++ src/components/Languages.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import * as languageServices from "../services/languageServices";
+
+const Languages = () => {
+  const [languages, setLanguages] = useState([]);
+  const [newLanguage, setNewLanguage] = useState("");
+
+  const fetchData = async () => {
+    const data = await languageServices.fetchLanguages();
+    console.log("Data:", data);
+    const newLanguagesState = data;
+    setLanguages(newLanguagesState);
+  };
+
+  const createLanguage = async () => {
+    try {
+      const data = await languageServices.createLanguage(newLanguage);
+      setNewLanguage("");
+    } catch (error) {
+      console.error("error creating language:", error.message);
+    }
+    fetchData();
+  };
+
+  const handleDeleteLanguage = async (language) => {
+    try {
+      await languageServices.deleteBook(language);
+    } catch (error) {
+      console.error("Error deleting language:", error.message);
+    }
+    fetchData();
+  };
+
+  function handleLanguageInput(e) {
+    setNewLanguage(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    createLanguage();
+  }
+
+  const languageList = languages.map((language, i) => {
+    return (
+      <li key={i}>
+        <ul>
+          <li>Language: {language.language}</li>
+          <li>Created at: {language.created_at}</li>
+          <li>
+            <button onClick={() => handleDeleteLanguage(language.language)}>
+              Delete
+            </button>
+          </li>
+        </ul>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h2>Language Data</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="newLanguage">New language:</label>
+        <input
+          id="newLanguage"
+          name="newLanguage"
+          onChange={handleLanguageInput}
+          value={newLanguage}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={fetchData}>Fetch</button>
+      <section>
+        <ul>{languageList}</ul>
+      </section>
+    </div>
+  );
+};
+
+export default Languages;
---
+++ src/services/languageServices.js
@@ -0,0 +1,54 @@
+const BASE_URL = import.meta.env.VITE_SERVER + "/lab/languages";
+
+const fetchLanguages = async () => {
+  try {
+    const res = await fetch(BASE_URL);
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error("Error fetching languages:", err.message);
+  }
+};
+
+const createLanguage = async (newLanguage) => {
+  const res = await fetch(BASE_URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      language: newLanguage,
+    }),
+  });
+  if (!res.ok) {
+    throw new Error("fail to add language");
+  }
+  const data = await res.json();
+  return data;
+};
+
+const deleteBook = async (language) => {
+  try {
+    const res = await fetch(BASE_URL + "/" + language, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!res.ok) {
+      const errorMsg = await res.text(); // get error message
+      throw new Error(`Cannot delete book: ${errorMsg || res.statusText}`);
+    }
+    return await res.json();
+  } catch (error) {
+    console.error("Error deleting language:", error.message);
+    throw error;
+  }
+};
+
+
+export { fetchLanguages, createLanguage, deleteBook };
